dfa8ab3446a3dd1b2b4cd21cbffc18af85799b8d
gadkari <gadkari@umich.edu>
2019-05-31 12:40:00 -0500

    Add BootstrapCDN to hello world message

--- firstproj/settings.py
+++ firstproj/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'firstproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['firstproj/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
